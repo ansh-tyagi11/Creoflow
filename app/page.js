5a973f1e554bf8251faff46cfb54345a79d26b3d
Ansh Tyagi <officialanshtyagi112@gmail.com>
2025-10-15 00:42:28 +0530

    Initial Commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Footer from "/components/Footer";
+import Navbar from "/components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
---
+++ components/SessionWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SessionWrapper = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SessionWrapper
