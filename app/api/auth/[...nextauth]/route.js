010d2f172e1fcab8f1c4434000a52453333706c3
Ansh Tyagi <officialanshtyagi112@gmail.com>
2025-10-29 07:33:11 +0530

    Updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,10 @@
-import React from "react"
 import NextAuth from "next-auth/next"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import UserSchema from "@/models/User"
+import connectDB from '@/db/connectDB'
 
-const handler = NextAuth({
+const handler = {
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,
@@ -13,6 +14,28 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET
         })
-    ]
-})
+    ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            if (account.provider == "github" || account.provider == "google") {
+                await connectDB()
+                const currentUser = await UserSchema.findOne({ email: email })
+                if (!currentUser) {
+                    const newUser = await UserSchema.create({
+                        email: user.email,
+                        username: user.username
+                    })
+                }
+                return true;
+            }
+        },
+
+        async session({ session, token, user }) {
+            const dbUser = await UserSchema.findOne({ email: session.user.email })
+            session.user.name = dbUser.username
+            return session
+        }
+    }
+}
+
 export { handler as GET, handler as POST }
